5e4d9d9fd1797413b65d12f5ea84bdc9dabfeb99
wimgonza <167358036+wimgonza@users.noreply.github.com>
2024-06-13 23:42:38 -0700

    I am adding a proper end screen that changes the scene.

--- src/Scenes/Platformer.js
+++ src/Scenes/Platformer.js
@@ -162,7 +162,8 @@ class Platformer extends Phaser.Scene {
         this.add.text(200, 25, "Collect all coins and get to the end", 20).setOrigin(0.5);
 
         if (this.coinCount == 0) {
-            this.add.text(2100, 170, "Game End. Hit R to restart.", 20).setOrigin(0.5);
+            //this.add.text(2100, 170, "Game End. Hit R to restart.", 20).setOrigin(0.5);
+            this.scene.start("endScreen");
         }
     }
 }
