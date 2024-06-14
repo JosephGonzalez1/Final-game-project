59982b5dbdf4e92943fe8609988296c15038f67f
JosephGonzalez1 <152119091+JosephGonzalez1@users.noreply.github.com>
2024-06-13 23:42:56 -0700

    adding end scene file
    I am adding the end scene file so that the player will have the end scene when they collect all of the coins.

---
+++ src/Scenes/EndScene.js
@@ -0,0 +1,20 @@
+class EndScene extends Phaser.Scene {
+    constructor() {
+        super("endScene");
+    }
+
+    preload() {
+
+    }
+
+    create() {
+        document.getElementById('description').innerHTML = '<h2>endScene.js</h2>'
+    }
+
+    update() {
+        if(Phaser.Input.Keyboard.JustDown(this.rKey)) {
+            this.scene.start("platformerScene");
+        }
+    }
+
+}
