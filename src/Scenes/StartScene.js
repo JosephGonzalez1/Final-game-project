c00e0ad3151056f800b0386002e751a15d0cfd6d
wimgonza <167358036+wimgonza@users.noreply.github.com>
2024-06-14 00:01:45 -0700

    Adding in the start button and rough title screen text that will be changed later.

--- src/Scenes/StartScene.js
+++ src/Scenes/StartScene.js
@@ -1,6 +1,7 @@
 class StartScene extends Phaser.Scene {
     constructor() {
         super("startScene");
+        this.spaceKey = null;
     }
 
     preload() {
@@ -9,10 +10,13 @@ class StartScene extends Phaser.Scene {
 
     create() {
         document.getElementById('description').innerHTML = '<h2>endScene.js</h2>'
+        this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
 
     update() {
-        if(Phaser.Input.Keyboard.JustDown(this.rKey)) {
+        this.add.text(200, 25, "Jumperman", 20).setOrigin(0.5);
+        this.add.text(200, 25, "Press space to start the game.", 20).setOrigin(0.5);
+        if(Phaser.Input.Keyboard.JustDown(this.spaceKey)) {
             this.scene.start("platformerScene");
         }
     }
