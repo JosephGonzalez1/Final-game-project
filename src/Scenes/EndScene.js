c6a4932a663d02f3e1706dea44222aa888e051e0
wimgonza <167358036+wimgonza@users.noreply.github.com>
2024-06-14 01:14:59 -0700

    Credits

--- src/Scenes/EndScene.js
+++ src/Scenes/EndScene.js
@@ -13,7 +13,9 @@ class EndScene extends Phaser.Scene {
 
     update() {
         this.add.text(750, 300, "Game over", 500).setOrigin(0.5);
-        this.add.text(750, 450, "Press T to restart the game.", 500).setOrigin(0.5);
+        this.add.text(750, 350, "Press T to restart the game.", 500).setOrigin(0.5);
+        this.add.text(750, 400, "Code and map by William and Joseph Gonzalez", 500).setOrigin(0.5);
+        this.add.text(750, 450, "Sprites and sounds created by Kenny Assets", 500).setOrigin(0.5);
         if(Phaser.Input.Keyboard.JustDown(this.tKey)) {
             this.scene.start("startScene");
         }
